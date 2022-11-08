2e1c4d4884dc5a5200f8580b8fca24ed38c73dad
chonhenry <chonhenry@gmail.com>
2022-11-07 19:24:11 -0500

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Henry Chon on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
