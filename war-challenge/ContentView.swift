54ddcfbe3a230e6c16d6267b4f16f4841c6e731e
chonhenry <chonhenry@gmail.com>
2022-11-11 18:12:39 -0500

    commit

--- war-challenge/ContentView.swift
+++ war-challenge/ContentView.swift
@@ -3,19 +3,86 @@
 //  war-challenge
 //
 //  Created by Henry Chon on 11/7/22.
-//
+//  xcrun simctl erase all
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack() {
+            Image("background").ignoresSafeArea()
+            
+            VStack() {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack() {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                
+                Button(action: {
+                    // generate a random number between 2 and 14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    // update the cards
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    // update the score
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    } else if cpuRand > playerRand{
+                        cpuScore += 1
+                    }
+                    
+                }, label: {
+                    Image("dealbutton")
+                })
+                
+                Spacer()
+                HStack() {
+                    Spacer()
+                    VStack() {
+                        Text("Player")
+                            .font(.headline)
+                        
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack() {
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }.foregroundColor(Color.white)
+                Spacer()
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewInterfaceOrientation(.portrait)
+        }
     }
 }
